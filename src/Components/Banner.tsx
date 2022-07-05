aa21f62132d2d6e1bbd684654832f6044e9bb0e9
Robert597 <robertseun1@gmail.com>
2022-07-05 19:01:07 -0400

    second commit

--- src/Components/Banner.tsx
+++ src/Components/Banner.tsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Banner = () => {
   return (
-    <div>Banner</div>
+    <section className='main'>
+        <div className="container">
+            <div className="row">
+                <h2>
+                    <div className="line">
+                        <span>Creating Unique Brand is</span>
+                    </div>
+                    <div className="line">
+                        <span>what we do at skates.</span>
+                    </div>
+                </h2>
+                <div className="btn-row">
+                    <a href="/">
+                        More About Us 
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
   )
 }
 
